f9f658a21267a8b80ae16a8f6e47f3ea77e3a883
Michael Bright <github.com@mjbright.net>
2018-09-25 09:10:26 +0200

    Finer checking on providers

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,6 +4,8 @@
 . setup_terraform_vars.rc 
 
 [ ! -d .terraform ] && terraform init
+find .terraform/plugins/ | grep -iq terraform-provider-digitalocean || terraform init
+find .terraform/plugins/ | grep -iq terraform-provider-null         || terraform init
 
 terraform apply |& tee -a terraform.apply.log
 
